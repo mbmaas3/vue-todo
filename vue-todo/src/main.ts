0d9b860a5462b3c1ad62a18f0a9e3bb055a1345d
mbmaas3 <65222746+mbmaas3@users.noreply.github.com>
2024-10-07 14:14:10 +0000

    Cleanup unused css files

--- vue-todo/src/main.ts
+++ vue-todo/src/main.ts
@@ -1,9 +1,8 @@
-import './assets/main.css'
+import './styles/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import './styles/index.css'
 
 const app = createApp(App)
 
