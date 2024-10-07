c37519276b4478311d4a67b1e1720af39d2918cc
mbmaas3 <65222746+mbmaas3@users.noreply.github.com>
2024-10-07 14:13:25 +0000

    Add possibility to run https on localhost chrome

--- vue-todo/vite.config.ts
+++ vue-todo/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { host: true },
   plugins: [
     vue(),
     vueDevTools(),
